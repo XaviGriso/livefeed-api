f1f4dad72a2ba4df6006519787808151eea5a435
Xavi <saverio.grisorio@gmail.com>
2020-02-25 20:43:56 +0000

    Endpoint in dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { ApolloServer } from 'apollo-server';
 import { prisma } from './generated/prisma-client';
 import { schema } from './schema';
+import * as env from 'dotenv';
 
+env.config();
 const server = new ApolloServer({
 	schema,
 	context: { prisma }
 });
+
+console.log(process.env.PRISMA_ENDPOINT);
 
 server.listen({ port: 4000 }, () =>
 	console.log(`🚀 - Server ready at http://localhost:4000`)
